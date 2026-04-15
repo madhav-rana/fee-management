a5b89f804741792d0d385923806a9daad4474c2a
Madhav Singh Rana <madhavsinghrana.college@gmail.com>
2026-04-15 09:01:23 +0530

    fix: some bugs related to flash msg

--- config/mailer.js
+++ config/mailer.js
@@ -1,14 +1,47 @@
-<<<<<<< HEAD
+// // const nodemailer = require("nodemailer");
+
+// // const transporter = nodemailer.createTransport({
+// //   host: "smtp.gmail.com",
+// //   port: 587,
+// //   secure: false,
+// //   auth: {
+// //     user: process.env.EMAIL_USER,
+// //     pass: process.env.EMAIL_PASS  //this must be App Password not your Gmail password
+// //   }
+// // });
+
+// // transporter.verify((err, success) => {
+// //   if (err) {
+// //     console.error("❌ SMTP ERROR:", err.message);
+// //   } else {
+// //     console.log("✅ Gmail SMTP ready");
+// //   }
+// // });
+
+
+// // module.exports = transporter;
+
+
+// // // If EMAIL_PASS is your actual Gmail password it won't work — Gmail requires an App Password (generated from Google Account → Security → 2FA → App Passwords)
+
+
+
 // const nodemailer = require("nodemailer");
 
 // const transporter = nodemailer.createTransport({
 //   host: "smtp.gmail.com",
-//   port: 587,
-//   secure: false,
+//   port: 465,
+//   secure: true,
 //   auth: {
 //     user: process.env.EMAIL_USER,
-//     pass: process.env.EMAIL_PASS  //this must be App Password not your Gmail password
-//   }
+//     pass: process.env.EMAIL_PASS,
+//   },
+//   tls: {
+//     rejectUnauthorized: false
+//   },
+//   connectionTimeout: 10000,
+//   greetingTimeout: 10000,
+//   socketTimeout: 15000,
 // });
 
 // transporter.verify((err, success) => {
@@ -19,57 +52,4 @@
 //   }
 // });
 
-
 // module.exports = transporter;
-
-
-// // If EMAIL_PASS is your actual Gmail password it won't work — Gmail requires an App Password (generated from Google Account → Security → 2FA → App Passwords)
-
-
-
-=======
->>>>>>> origin/Siddharth
-const nodemailer = require("nodemailer");
-
-const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-<<<<<<< HEAD
-  port: 465,
-  secure: true,
-  auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
-  },
-  tls: {
-    rejectUnauthorized: false
-  },
-  connectionTimeout: 10000,
-  greetingTimeout: 10000,
-  socketTimeout: 15000,
-=======
-  port: 587,
-  secure: false,
-  auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS  //this must be App Password not your Gmail password
-  }
->>>>>>> origin/Siddharth
-});
-
-transporter.verify((err, success) => {
-  if (err) {
-    console.error("❌ SMTP ERROR:", err.message);
-  } else {
-    console.log("✅ Gmail SMTP ready");
-  }
-});
-
-<<<<<<< HEAD
-module.exports = transporter;
-=======
-
-module.exports = transporter;
-
-
-// If EMAIL_PASS is your actual Gmail password it won't work — Gmail requires an App Password (generated from Google Account → Security → 2FA → App Passwords)
->>>>>>> origin/Siddharth
