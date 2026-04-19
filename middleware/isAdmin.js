294f20f771ec45e04e1c6cd28908baed239e35fa
Madhav Singh Rana <madhavsinghrana.college@gmail.com>
2026-04-19 14:08:48 +0530

    fix: isAdmin to handle undefined session

--- middleware/isAdmin.js
+++ middleware/isAdmin.js
@@ -1,5 +1,6 @@
 module.exports = (req, res, next) => {
-  if (!req.session.admin.id) {
+  // if (!req.session.admin.id) { // crashes if req.session.admin is undefined
+  if (!req.session.admin || !req.session.admin.id) {
     return res.redirect("/api/v1/admin/login");
   }
   next();
