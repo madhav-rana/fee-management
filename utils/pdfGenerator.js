de2ae8ff639f42488cc84f4c8664b90151af3e78
Madhav Singh Rana <madhavsinghrana.college@gmail.com>
2026-04-27 12:16:44 +0530

    replace puppeteer with html-pdf-node for report PDF generation

--- utils/pdfGenerator.js
+++ utils/pdfGenerator.js
@@ -1,65 +1,12 @@
-// const puppeteer = require("puppeteer");
-// const ejs = require("ejs");
-// const path = require("path");
-
-// async function generatePDF(viewPath, data) {
-//   const browser = await puppeteer.launch({
-//     headless: "new"
-//   });
-
-//   const page = await browser.newPage();
-
-//   // Render EJS to HTML
-//   const html = await ejs.renderFile(viewPath, data);
-
-//   await page.setContent(html, {
-//     waitUntil: "networkidle0"
-//   });
-
-//   const pdfBuffer = await page.pdf({
-//     format: "A4",
-//     printBackground: true,
-//     margin: {
-//       top: "20px",
-//       bottom: "20px",
-//       left: "20px",
-//       right: "20px"
-//     }
-//   });
-
-//   await browser.close();
-//   return pdfBuffer;
-// }
-
-// module.exports = generatePDF;
-
-
-
-const puppeteer = require("puppeteer");
+const htmlPdf = require("html-pdf-node");
 const ejs = require("ejs");
-const path = require("path");
 
 async function generatePDF(viewPath, data) {
-  const browser = await puppeteer.launch({
-    headless: "new",
-    executablePath: process.env.PUPPETEER_EXECUTABLE_PATH || undefined,
-    args: [
-      "--no-sandbox",
-      "--disable-setuid-sandbox",
-      "--disable-dev-shm-usage",
-      "--disable-gpu",
-    ]
-  });
-
-  const page = await browser.newPage();
-
   const html = await ejs.renderFile(viewPath, data);
 
-  await page.setContent(html, {
-    waitUntil: "networkidle0"
-  });
+  const file = { content: html };
 
-  const pdfBuffer = await page.pdf({
+  const options = {
     format: "A4",
     printBackground: true,
     margin: {
@@ -68,9 +15,9 @@ async function generatePDF(viewPath, data) {
       left: "20px",
       right: "20px"
     }
-  });
+  };
 
-  await browser.close();
+  const pdfBuffer = await htmlPdf.generatePdf(file, options);
   return pdfBuffer;
 }
 
